6588b6f10e20bcdc715ef896d1aef4d3c2875071
StevensLo <stevenslouis@phacs.net>
2022-02-26 17:28:38 -0500

    1 file modified

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,6 +2,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Hi Stevens!</h1>
+      <h1>Hello!</h1>
       <h1>Cool Amazon Price Tracking App</h1>
       <ul>
         <li>
