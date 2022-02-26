4be7ec16212cbd5eb6c42d412a8151b2c6ce8de5
liangtfm <liangtfm@gmail.com>
2022-02-26 17:14:54 -0500

    1 file modified

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,7 +2,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Hi Stevens!</h1>
-      <h1>Welcome to Remix</h1>
+      <h1>Cool Amazon Price Tracking App</h1>
       <ul>
         <li>
           <a
