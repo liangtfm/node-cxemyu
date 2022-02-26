aad9b76524523137d9daa69b5f9d4dcd80f6fc2b
liangtfm <liangtfm@gmail.com>
2022-02-26 17:38:36 -0500

    1 file modified

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,14 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Hi Stevens!</h1>
-      <h1>Hello!</h1>
-      <h2>Let's gooo!</h2>
       <h1>Cool Amazon Price Tracking App</h1>
+      <h1>Stevens Todo List</h1>
+      <ol>
+        <li>Text input field for the Amazon link</li>
+        <li>Submit button for the Text Input field</li>
+        <li>Render a list of the items we are tracking</li>
+      </ol>
+      <h2>Helpful links</h2>
       <ul>
         <li>
           <a
@@ -29,6 +33,9 @@ export default function Index() {
             Remix Docs
           </a>
         </li>
+        <li>
+          <a target="_blank" href="https://reactjs.org/docs/getting-started.html">React Docs</a>
+          </li>
       </ul>
     </div>
   );
